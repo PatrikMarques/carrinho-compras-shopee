cba81362df6dc1a89418c4ecba3c4d212ca7d465
Patrik Marques <patriklancador01@gmail.com>
2025-07-16 22:19:11 -0300

    Display Cart

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as cartService from "./services/cart.js";
+import createItem from "./services/item.js";
+
+const myCart = [];
+const myWhishList = [];
+
+console.log("welcome to the your Shopee Cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+
+//adicionei dois itens ao carrinho
+await cartService.additem(myCart, item1);
+await cartService.additem(myCart, item2);
+
+await cartService.displayCart(myCart);
+//deletei dois itens do carrinho
+//await cartService.deleteItem(myCart, item2.name);
+//await cartService.deleteItem(myCart, item1.name);
+
+
+await cartService.calculateTotal(myCart);
---
+++ src/services/cart.js
@@ -0,0 +1,43 @@
+//quais ações meu carrinho pode fazer?
+
+//casos de uso
+//-> adicionar item no carrinho
+async function additem(userCart, item) {
+    userCart.push(item);
+}
+
+//-> deletar item do carrinho
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
+}
+
+//-> remover um item - diminui um item
+async function removeItem(userCart, index) {
+    
+}
+
+//-> calcular o total do carrinho
+async function calculateTotal(userCart) {
+    console.log("\nShopee Cart TOTAL IS:");
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+    console.log(`Total: ${result}`);
+} 
+
+async function displayCart(userCart) {
+    console.log("\nShopee cart list:");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantily}x | Subtotal = ${item.subtotal()}`);
+    });
+}
+
+export {
+    additem, 
+    calculateTotal,
+    deleteItem,
+    removeItem, 
+    displayCart,
+} 
